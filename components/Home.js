efe785b64b036c15a08ebdf9e802fd03e1df9b06
Jb <deslandes.jb@gmail.com>
2025-12-05 18:16:38 +0100

    shadcn added

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,22 @@
-import styles from '../styles/Home.module.css';
+// import styles from '../styles/Home.module.css';
+import {Button} from '@/components/ui/button';
 
 function Home() {
-  return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
-    </div>
-  );
+	return (
+		<div>
+			<main className="flex h-screen flex-col items-center justify-center bg-red-100">
+				<section>
+					<h1 className="italic">
+						Welcome to <a href="https://nextjs.org">Next.js!</a>
+					</h1>
+				</section>
+
+				<section>
+					<Button>Shadcn test</Button>
+				</section>
+			</main>
+		</div>
+	);
 }
 
 export default Home;
