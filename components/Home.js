9a4bb4c40b193b9e2137df503ef5511cabad0e46
Jb <deslandes.jb@gmail.com>
2025-12-08 11:15:41 +0100

    header component

--- components/Home.js
+++ components/Home.js
@@ -4,7 +4,7 @@ import {Button} from '@/components/ui/button';
 function Home() {
 	return (
 		<>
-			<main className="flex h-screen-header flex-col items-center justify-center font-body">
+			<main className="flex h-screen-header min-h-96 flex-col items-center justify-center font-body">
 				<section>
 					<h1 className="italic">
 						Welcome to <a href="https://nextjs.org">Next.js!</a>
@@ -12,7 +12,7 @@ function Home() {
 				</section>
 
 				<section>
-					<Button className="bg-orange text-zinc-900 hover:bg-orangehover">Shadcn component</Button>
+					<Button className="bg-orange text-zinc-900 shadow-sm hover:bg-orangehover">Shadcn component</Button>
 				</section>
 			</main>
 		</>
--- components/global/Header.js
+++ components/global/Header.js
@@ -1,5 +1,5 @@
 import {Button} from '@/components/ui/button';
-import {Menu, X} from 'lucide-react';
+import {ArrowLeft, Menu, X} from 'lucide-react';
 import Link from 'next/link';
 
 // drawer
@@ -14,9 +14,20 @@ import {
 	DrawerTrigger,
 } from '@/components/ui/drawer';
 
+import {
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuIndicator,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+	NavigationMenuViewport,
+} from '@/components/ui/navigation-menu';
+
 export default function Header() {
 	return (
-		<header className="flex h-16 items-center justify-between bg-lightblue px-4 font-title">
+		<header className="flex h-16 items-center justify-between bg-lightblue px-4 font-title shadow-sm">
 			<div>
 				<Link href="/">logo</Link>
 			</div>
@@ -30,24 +41,101 @@ export default function Header() {
 						<DrawerContent>
 							<DrawerHeader>
 								<DrawerTitle>Logo</DrawerTitle>
+								<DrawerDescription>Main menu</DrawerDescription>
 							</DrawerHeader>
 							<div className="flex h-screen flex-col">
 								<DrawerClose className="mr-4 h-16 w-fit self-end">
 									<X />
 								</DrawerClose>
-								<nav className="flex h-full flex-col items-center justify-center">
-									<Link href="#">test</Link>
-									<Link href="#">test</Link>
+								<nav className="flex h-full flex-col items-center justify-center gap-4">
+									<Link className="lowercase first-letter:uppercase" href="#">
+										test
+									</Link>
+									<Link className="lowercase first-letter:uppercase" href="#">
+										test
+									</Link>
+									{/* NESTED */}
+									<Drawer direction="right">
+										<DrawerTrigger className="lowercase first-letter:uppercase">All categories</DrawerTrigger>
+										<DrawerContent>
+											<DrawerHeader>
+												<DrawerTitle>Logo</DrawerTitle>
+												<DrawerDescription>Second menu</DrawerDescription>
+											</DrawerHeader>
+											<div className="flex h-screen flex-col">
+												<DrawerClose className="mr-4 h-16 w-fit self-end">
+													<ArrowLeft />
+												</DrawerClose>
+												<nav className="flex h-full flex-col items-center justify-center gap-4">
+													<Link className="lowercase first-letter:uppercase" href="#">
+														test
+													</Link>
+													<Link className="lowercase first-letter:uppercase" href="#">
+														test
+													</Link>
+												</nav>
+											</div>
+										</DrawerContent>
+									</Drawer>
 								</nav>
 							</div>
 						</DrawerContent>
 					</Drawer>
 				</div>
 				{/* desktop */}
-				<nav className="hidden gap-4 md:flex">
-					<Link href="#">test1</Link>
-					<Link href="#">test2</Link>
-				</nav>
+				<NavigationMenu className="hidden md:flex">
+					<NavigationMenuList className="flex-wrap gap-4">
+						<NavigationMenuItem>
+							<NavigationMenuTrigger className="bg-transparent p-0">All categories</NavigationMenuTrigger>
+							<NavigationMenuContent>
+								<ul className="grid gap-2 p-4 md:w-[400px] lg:w-[600px] lg:grid-cols-[.75fr_1fr]">
+									<li className="row-span-3">
+										<NavigationMenuLink asChild>
+											<a
+												className="bg-linear-to-b outline-hidden flex h-full w-full select-none flex-col justify-end rounded-md from-muted/50 to-muted p-4 no-underline transition-all duration-200 focus:shadow-md md:p-6"
+												href="/">
+												<div className="mb-2 text-lg font-medium sm:mt-4">shadcn/ui</div>
+												<p className="text-sm leading-tight text-muted-foreground">
+													Beautifully designed components built with Tailwind CSS.
+												</p>
+											</a>
+										</NavigationMenuLink>
+									</li>
+									<li className="flex flex-col gap-2">
+										<Link href="#" className="inline-block">
+											Test
+										</Link>
+										<Link href="#" className="inline-block">
+											Test
+										</Link>
+										<Link href="#" className="inline-block">
+											Test
+										</Link>
+										<Link href="#" className="inline-block">
+											Test
+										</Link>
+										<Link href="#" className="inline-block">
+											Test
+										</Link>
+									</li>
+								</ul>
+							</NavigationMenuContent>
+						</NavigationMenuItem>
+						<NavigationMenuItem>
+							<NavigationMenuLink asChild>
+								<Link href="#">Test</Link>
+							</NavigationMenuLink>
+						</NavigationMenuItem>
+						<NavigationMenuItem>
+							<NavigationMenuLink asChild>
+								<Link href="#">Test</Link>
+							</NavigationMenuLink>
+						</NavigationMenuItem>
+					</NavigationMenuList>
+				</NavigationMenu>
+			</div>
+			<div>
+				<Link href="#">Connexion</Link>
 			</div>
 		</header>
 	);
