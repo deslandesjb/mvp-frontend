2026b6dbf12b85aab6bde7290fa9e1b37dc548f6
Jb <deslandes.jb@gmail.com>
2025-12-10 16:24:09 +0100

    add products pages & start to sho all products on the page

---
+++ pages/allproducts.js
@@ -0,0 +1,7 @@
+import AllProducts from '../components/AllProducts';
+
+function AllProductsPage() {
+	return <AllProducts />;
+}
+
+export default AllProductsPage;
---
+++ pages/category.js
@@ -0,0 +1,7 @@
+import Category from '../components/Category';
+
+function CategoryPage() {
+	return <Category />;
+}
+
+export default CategoryPage;
