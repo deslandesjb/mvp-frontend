7800128429062086b2593e5849d4c6517e1e44d1
Jb <deslandes.jb@gmail.com>
2025-12-12 10:47:36 +0100

    next.config domains change with new nextjs

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const nextConfig = {
 	reactStrictMode: true,
 	devIndicators: false,
 	images: {
-		domains: ['static.fnac-static.com'],
+		// domains: ['static.fnac-static.com'],
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'static.fnac-static.com',
+				pathname: '**',
+			},
+		],
 	},
 };
 
