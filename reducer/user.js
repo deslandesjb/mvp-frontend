ec7a564e92e65545fd8e9d5859e1cddfe565ee9f
Azeddine <azederg@gmail.com>
2025-12-15 14:52:17 +0100

    mise a jour des token

--- reducer/user.js
+++ reducer/user.js
@@ -1,30 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
+  import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-  token: null,
-  firstname: null,
-  lastname: null,
-  mail: null,
-};
+  const initialState = {
+    token: null,
+    firstname: null,
+    lastname: null,
+    mail: null,
+  };
 
-const userSlice = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {
-    login: (state, action) => {
-      state.token = action.payload.token;
-      state.firstname = action.payload.firstname;
-      state.lastname = action.payload.lastname;
-      state.mail = action.payload.mail;
+  const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+      login: (state, action) => {
+        state.token = action.payload.token;
+        state.firstname = action.payload.firstname;
+        state.lastname = action.payload.lastname;
+        state.mail = action.payload.mail;
+      },
+      logout: (state) => {
+        state.token = null;
+        state.firstname = null;
+        state.lastname = null;
+        state.mail = null;
+      },
     },
-    logout: (state) => {
-      state.token = null;
-      state.firstname = null;
-      state.lastname = null;
-      state.mail = null;
-    },
-  },
-});
+  });
 
-export const { login, logout } = userSlice.actions;
-export default userSlice.reducer;
+  export const { login, logout } = userSlice.actions;
+  export default userSlice.reducer;
